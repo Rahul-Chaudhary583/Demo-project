d1b45b91b07f82f52370f95a95c69ccecabae070
Rahul <Rahul@DESKTOP-9CGPFRI>
2020-04-16 18:35:03 +0530

    Rahul

---
+++ Add Two Number/Add Two Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Two_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Firstnumber, SecondNumber, Result;
+            Console.WriteLine("Enter your first Number");
+            Firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your Second Number");
+            SecondNumber  = int.Parse(Console.ReadLine());
+
+            Result = Firstnumber + SecondNumber;
+            Console.WriteLine("The Sum Of two number is: " + Result);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
